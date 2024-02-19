c8d456dadb49335353920d9aa316326475140d3d
barnarddale <dale@dalebarnard.com>
2024-02-19 18:17:13 -0500

    tidying up

--- portfile.cmake
+++ portfile.cmake
@@ -1,7 +1,9 @@
+#include(vcpkg_common_functions)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO barnarddale/try-catcher
-    REF 392ff08524932bd781c06c80a2f5cc3efe34d066
+    REF f8f6bf553efae7d808ff37d473cbcbf4b1ba0a5d
     SHA512 0
     HEAD_REF main
 )
